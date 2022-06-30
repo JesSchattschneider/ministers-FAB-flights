f3cf061232ae1b678b1200d2f388ff674ef49661
jessica leiria schattschneider <jls@pop-os.localdomain>
2022-06-30 17:37:59 +1200

    breaking down grooming

--- utils.py
+++ utils.py
@@ -2,12 +2,16 @@ import glob
 import tabula
 import pandas as pd
 from skimpy import clean_columns
+from unidecode import unidecode
+
 
 class Wrangling():
     # find all pdfs downloaded from the extract script
     list_pdfs = glob.glob("pdfs/*.pdf")
 
     # function to find and save tables from pdfs
+    # I had one issue reading file 20130819_170303.pdf, so I removed this file from the folder. 
+    # In the future the function below needs to handle exeptions and skip and/or log problematic files. 
     def save_tables_from_pdfs(list_pdfs):
 
         # extract tables from pdf
@@ -44,3 +48,26 @@ class Wrangling():
 
         # save res to csv
         pd.DataFrame(res).to_csv("all_data.csv")
+
+    def clean_flights(data, cols):
+        # select specific cols
+        cols = ["autoridades_apoiadas","origem","decolagem_h_local","destino","pouso_h_local","motivo","previsao_de_passageiros","file_name"]
+        data = data[cols] # select specific cols
+
+        # remove occurences without departing time
+        # Convert NaN values to empty string
+        nan_value = float("NaN")
+        data.replace("", nan_value, inplace=True)
+        data.dropna(subset = ['decolagem_h_local', 'origem', 'destino'], inplace=True)
+
+        # groom columns destino and origem
+        cols_clean = ['origem', 'destino']
+        for col_clean in cols_clean:
+            data[col_clean] = data[col_clean].astype(str) # column should be type string
+            data[col_clean] = data[col_clean].str.replace(r"\(.*\)","") # remove everything that is inside () (inclusive)
+            data[col_clean] = data[col_clean].str.strip() # remove extra empty spaces
+            data[col_clean] = data[col_clean].apply(unidecode) # replace letters with accents with the letter without accent.
+            data = data[~data[col_clean].str.contains("/")] # if any city contains : it will be removed - avoid problematic 
+                                                            # records with data instead of cities in origem or destino
+
+        return(data)
